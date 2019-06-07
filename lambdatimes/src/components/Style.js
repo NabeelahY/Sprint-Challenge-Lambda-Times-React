ab49fc9138b61cf8e0567b93b3378e533cf72133
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-07 10:39:39 +0100

    Styled Header with styled components

--- lambdatimes/src/components/Style.js
+++ lambdatimes/src/components/Style.js
@@ -69,3 +69,45 @@ export const Right = styled(Center)`
   }
 `;
 
+export const HeaderSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+
+  h1 {
+    font-size: 60px;
+    font-family: Didot, serif;
+    font-weight: bold;
+    flex: 8;
+    text-align: center;
+    color: #000;
+  }
+
+  span {
+    align-self: flex-end;
+    font-size: 11px;
+    font-weight: bold;
+    letter-spacing: 1px;
+  }
+`;
+
+export const Date = styled.div`
+  margin-left: 25px;
+  flex: 1;
+`;
+
+export const Temp = styled(Date)`
+  text-align: right;
+  margin-right: 25px;
+`;
